bf8129e6a4460d06ba50c502f9133cdc15d7ed6b
protatodev <thaddonaghue@yahoo.com>
2018-06-27 11:55:30 -0700

    Add project files.

---
+++ CoinCombination/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoinCombination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
